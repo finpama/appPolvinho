0b9a48de4eab800079c1c27fc7e9c931338f33ea
finpama <phma020@gmail.com>
2024-11-06 05:51:16 -0300

    Melhoramento do error handling

--- src/AppComponents/ErrorPage/ErrorPage.jsx
+++ src/AppComponents/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,17 @@
+import { useRouteError } from 'react-router-dom'
+
 function ErrorPage(){
+	const error = useRouteError()
+
+	console.log(error)
+
 	return <>
-		<h1>ERRO</h1>
+		<h1>
+			ERRO: <br /> <br /> 
+			{error.status}, {error.statusText}
+		</h1>
+		<br />
+		<p>{error.data}</p>
 	</>
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import AppRouter from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<RouterProvider router={AppRouter} />,
+	<StrictMode>
+		<RouterProvider router={AppRouter} />
+	</StrictMode>
 );
