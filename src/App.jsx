a538a2b0ff11769afeaf254bf73a114eb68d2a00
finpama <phma020@gmail.com>
2024-11-24 12:27:46 -0300

    Melhores elements e re-colocando errorElement

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './Assets/rootTools.scss'
 import logoFull from './Assets/Logo_full.png'
 
 import Navbar from './AppComponents/Navbar/Navbar.jsx'
+
 import ErrorPage from './AppComponents/ErrorPage/ErrorPage.jsx'
 import LoginPage from './AppComponents/LoginPage/LoginPage.jsx'
 import AccountPage from './AppComponents/AccountPage/AccountPage.jsx'
@@ -13,30 +14,20 @@ import { createBrowserRouter } from 'react-router-dom';
 const routesConfig = [
 	{
 		path: '/',
-		element: <>
-				<LoginPage/>
-		</>
+		element: <LoginPage/>,
+		errorElement: <ErrorPage/>
 	},
 	{
 		path: '/portugues',
-		element: 
-			<>
-				<PortuguesePage/>
-			</>
+		element: <PortuguesePage/>
 	},
 	{
 		path: '/matematica',
-		element: 
-			<>
-				<MathPage/>
-			</>
+		element: <MathPage/>
 	},
 	{
 		path: '/conta',
-		element: 
-			<>
-				<AccountPage/>
-			</>
+		element: <AccountPage/>
 	},
 
 ]
